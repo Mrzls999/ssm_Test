645ce5ab542d94811b8962e59a9e140d015f2e5d
zls <mrzxs999@163.com>
2021-09-06 14:49:39 +0800

    update all

---
+++ ssm_springTest1/src/ssm/example/pojo/Employee.java
@@ -0,0 +1,43 @@
+package ssm.example.pojo;
+
+/**
+ * @author zls
+ * @date 2021/9/6 9:26:45
+ * @description XXX
+ */
+public class Employee {
+    private String id;
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Employee() {
+    }
+
+    public Employee(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ ssm_springTest1/src/ssm/example/test/SpringTest.java
@@ -0,0 +1,20 @@
+package ssm.example.test;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ssm.example.pojo.Employee;
+
+/**
+ * @author zls
+ * @date 2021/9/6 9:30:54
+ * @description XXX
+ */
+public class SpringTest {
+    @Test
+    public void testInit(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+        Employee bean = context.getBean("emp1",Employee.class);
+        System.out.println(bean);
+    }
+}
